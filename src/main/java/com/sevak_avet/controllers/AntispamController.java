0cd308851ee16198dca58eecd8cc91c8e46e9db9
Sevak Avetisyan <sevak.avet@gmail.com>
2015-07-07 01:37:15 +0300

    Anti-spam page empty controller

--- src/main/java/com/sevak_avet/controllers/AntispamController.java
+++ src/main/java/com/sevak_avet/controllers/AntispamController.java
@@ -1,19 +1,22 @@
 package com.sevak_avet.controllers;
 
 import com.sevak_avet.dao.AntiSpamDao;
-import com.sevak_avet.dao.AutoApproveDao;
 import com.sevak_avet.dao.UserDao;
-import com.sevak_avet.domain.AntiSpam;
 import org.apache.log4j.Logger;
 import org.jinstagram.Instagram;
+import org.jinstagram.entity.users.feed.UserFeedData;
+import org.jinstagram.exceptions.InstagramException;
+import org.jinstagram.model.Relationship;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 /**
  * Created by savetisyan on 05.07.15.
@@ -31,7 +34,9 @@ public class AntispamController {
     private AntiSpamDao antiSpamDao;
 
     @RequestMapping(method = RequestMethod.GET)
-    public String get(HttpSession session, ModelMap params) {
+    public String get(HttpSession session) {
+        log.info("Antispam page GET");
+
         Instagram instagram = (Instagram) session.getAttribute("instagram");
         if (instagram == null) {
             return "redirect:login";
@@ -39,4 +44,11 @@ public class AntispamController {
 
         return "antispam";
     }
+
+    @RequestMapping(params = "saveAntiSpam", method = RequestMethod.POST)
+    public String saveBadWords(@RequestParam("badWordsList") String badWordsList) {
+        log.info(badWordsList);
+        return "redirect:antispam";
+    }
 }
+
